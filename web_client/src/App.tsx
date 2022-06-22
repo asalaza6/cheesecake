fbdc6055bda915926f82714de434d1b00b52aa2f
Alex Salazar <55558620+asalaza6@users.noreply.github.com>
2022-06-22 16:43:13 -0700

    Add motion between pathnames

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -13,6 +13,7 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { LandingPage, Dev1, Dev2, Dev3, Dev4, Buy } from './screens';
+import { AnimatePresence } from 'framer-motion';
 
 toast.configure()
 
@@ -25,14 +26,16 @@ function App(props: any) {
   
   return (
     <Router>
-      <Switch>
-        <Route exact path = "/" render={LandingPage}/>
-        <Route exact path = "/buy" render={Buy}/>
-        <Route exact path = "/dev1" render={Dev1}/>
-        <Route exact path = "/dev2" render={Dev2}/>
-        <Route exact path = "/dev3" render={Dev3}/>
-        <Route exact path = "/dev4" render={Dev4}/>
-      </Switch>
+      <AnimatePresence>
+        <Switch>
+          <Route exact path = "/" render={LandingPage}/>
+          <Route exact path = "/buy" render={Buy}/>
+          <Route exact path = "/dev1" render={Dev1}/>
+          <Route exact path = "/dev2" render={Dev2}/>
+          <Route exact path = "/dev3" render={Dev3}/>
+          <Route exact path = "/dev4" render={Dev4}/>
+        </Switch>
+      </AnimatePresence>
     </Router>
   );
 }
--- web_client/src/components/Layout.tsx
+++ web_client/src/components/Layout.tsx
@@ -1,6 +1,21 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { Header } from './Header';
 import './styling.css';
+const pageVariants = {
+    initial: {
+        opacity: 0,
+        x: 300,
+    },
+    in: {
+        opacity: 1,
+        x: 0,
+    },
+    out: {
+        opacity: 0,
+        x: -300,
+    },
+}
 
 export const Layout: React.FC<any> = (props: any) => {
 
@@ -8,15 +23,22 @@ export const Layout: React.FC<any> = (props: any) => {
         children
     } = props;
 
+    const pathname = window.location.pathname;
+
     const divProps = {
         className: 'Layout',
-    }
+        initial: 'initial',
+        animate: 'in',
+        exit: 'out',
+        variants: pageVariants,
+        key: pathname,
+    };
     
     return (
-        <div {...divProps}>
+        <motion.div {...divProps}>
             <Header />
             {children}
-        </div>
+        </motion.div>
     );
 }
 
