35aa4d13b3a7c5ea512e4b4258298a42da13ce94
Alex Salazar <55558620+asalaza6@users.noreply.github.com>
2022-08-12 10:51:34 -0700

    Update email.ts

--- cheesecake-next/pages/api/email.ts
+++ cheesecake-next/pages/api/email.ts
@@ -1,5 +1,23 @@
 import nodemailer from 'nodemailer';
 
+const transportSendMailAsync = (transport: any, mailOptions): Promise<{ success: boolean, data: any }> => {
+    return new Promise((resolve, reject) => {
+        transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                resolve({
+                    success: false,
+                    data: error,
+                });
+            } else {
+                resolve({
+                    success: true,
+                    data: info,
+                });
+            }
+        })
+    });
+}
+
 export default async function handle(req, res) {
     try{
         const { 
@@ -25,17 +43,12 @@ export default async function handle(req, res) {
             text: JSON.stringify(products),
             html: JSON.stringify(products),
         };
-        
-        transport.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                throw error;
-            }
-            return res.json(info);
-        })
+        const emailRes = await transportSendMailAsync(transport, mailOptions);
+        res.json(emailRes);
 
         // send email to user 
         // send email to business
-        return res.json(true);
+        // return res.json(true);
     } catch(err: any){
         console.log(err.message);
         res.status(500).send("Server Error");
